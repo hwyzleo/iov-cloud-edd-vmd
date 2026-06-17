240dc9088f6ec3ad727c2c94ec57ddeb7caee70b
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-06-17 22:24:02 +0800

    refactor(vmd): clean up unused imports and dependencies in TolEcuListParserV1_0

--- edd-vmd-service/src/main/java/net/hwyz/iov/cloud/edd/vmd/service/application/vid/impl/TolEcuListParserV1_0.java
+++ edd-vmd-service/src/main/java/net/hwyz/iov/cloud/edd/vmd/service/application/vid/impl/TolEcuListParserV1_0.java
@@ -1,23 +1,11 @@
 package net.hwyz.iov.cloud.edd.vmd.service.application.vid.impl;
 
-import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
-import cn.hutool.json.JSONUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.hwyz.iov.cloud.edd.vmd.service.application.dto.result.ImportResult;
-import net.hwyz.iov.cloud.edd.vmd.service.application.service.PartInfoAppService;
-import net.hwyz.iov.cloud.edd.vmd.service.application.service.VehiclePartAppService;
 import net.hwyz.iov.cloud.edd.vmd.service.application.vid.ImportDataParserRegistry;
 import net.hwyz.iov.cloud.edd.vmd.service.application.vid.VehicleImportDataParser;
-import net.hwyz.iov.cloud.edd.vmd.service.common.exception.PartBindingConflictException;
-import net.hwyz.iov.cloud.edd.vmd.service.common.exception.VehicleNotExistException;
-import net.hwyz.iov.cloud.edd.vmd.service.domain.model.entity.PartInfo;
-import net.hwyz.iov.cloud.edd.vmd.service.domain.model.entity.VehicleBasicInfo;
-import net.hwyz.iov.cloud.edd.vmd.service.domain.model.entity.VehiclePart;
-import net.hwyz.iov.cloud.edd.vmd.service.domain.model.valueobject.PartInstanceState;
-import net.hwyz.iov.cloud.edd.vmd.service.domain.repository.VehBasicInfoRepository;
-import net.hwyz.iov.cloud.framework.common.util.StrUtil;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
@@ -37,9 +25,6 @@ import jakarta.annotation.PostConstruct;
 public class TolEcuListParserV1_0 extends BaseProcessor implements VehicleImportDataParser {
 
     private final ImportDataParserRegistry parserRegistry;
-    private final VehBasicInfoRepository vehBasicInfoRepository;
-    private final PartInfoAppService partInfoAppService;
-    private final VehiclePartAppService vehiclePartAppService;
 
     @PostConstruct
     public void init() {
@@ -58,7 +43,6 @@ public class TolEcuListParserV1_0 extends BaseProcessor implements VehicleImport
 
     @Override
     public ImportResult parse(String batchNum, JSONObject dataJson) {
-        // TODO: 实现解析逻辑
-        return ImportResult.builder().build();
+        throw new UnsupportedOperationException("TODO: TOL parser not yet implemented");
     }
 }
